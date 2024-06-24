a6a9dfd2e19de67bf45439f03da489697637b257
Andy Martinez <andymartinezot@gmail.com>
2024-06-24 16:43:08 -0300

    Add configurations to use docker-compose

--- backend/config/config.go
+++ backend/config/config.go
@@ -22,15 +22,3 @@ func ConnectionDB() *sql.DB {
 	}
 	return connection
 }
-
-// func ConnectionDB() *sql.DB {
-// 	Driver := "mysql"
-// 	User := "root"
-// 	Pass := "password123"
-// 	Name := "system"
-
-// 	connection, err := sql.Open(Driver, User+":"+Pass+"@tcp(127.0.0.1)/"+Name)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	return connection
